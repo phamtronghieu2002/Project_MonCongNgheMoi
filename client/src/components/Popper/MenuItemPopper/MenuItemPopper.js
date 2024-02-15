3ee1977e62d6204258516d3575eb43cfe4e7065d
Nhan Vien B <20062501.hieu@student.iuh.edu.vn>
2024-02-15 21:21:11 +0700

    fix bug floating popoper

--- client/src/components/Popper/MenuItemPopper/MenuItemPopper.js
+++ client/src/components/Popper/MenuItemPopper/MenuItemPopper.js
@@ -1,24 +1,20 @@
-import "./MenuItemPopper.scss"
-import clsx from "clsx";
-function ManuItemPopper({Icon,title,subMenu,callback,titleStyle}) {
+import './MenuItemPopper.scss';
+import clsx from 'clsx';
+function ManuItemPopper({ Icon, title, subMenu, callback, titleStyle }) {
     return (
-        <div 
-        onClick={callback}
-        className='menu_item_poper position-relative'>
-                <span className="me-2">  {Icon && Icon} </span>
-                <span className={titleStyle}>{title}</span>
-                {subMenu && <i
-                style={{position:"absolute",right:"10px"}}
-                className="fa-solid fa-chevron-right"></i>}
-                {
-                    subMenu && <div className="sub_menu_poper">
-                        {subMenu.map((item,index)=>(
-                            <ManuItemPopper key={index} {...item}  />
-                        ))}
-                    </div>
-                }
+        <div onClick={callback} className="menu_item_poper position-relative">
+            <span className="me-2"> {Icon && Icon} </span>
+            <span className={titleStyle}>{title}</span>
+            {subMenu && <i style={{ position: 'absolute', right: '10px' }} className="fa-solid fa-chevron-right"></i>}
+            {subMenu && (
+                <div className="sub_menu_poper">
+                    {subMenu.map((item, index) => (
+                        <ManuItemPopper key={index} {...item} />
+                    ))}
+                </div>
+            )}
         </div>
-      );
+    );
 }
 
 export default ManuItemPopper;
